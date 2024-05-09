30c49eece3f0a7913379deead929d548dd1739e1
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2024-05-09 19:19:30 +0200

    feat(logic): calculator now properly calculates
    After pressing "=" calculator now calculates and displays result.

--- script.js
+++ script.js
@@ -31,7 +31,9 @@ function operate(target) {
   if (target.id === "clear") {
     clear();
   } else if (target.id === "equals") {
-    newOperation(target);
+    calculate();
+    clearDisplay();
+    updateDisplay(calculator.currentNum);
   } else {
     newOperation(target);
   }
@@ -43,6 +45,49 @@ function newOperation(target) {
     calculator.cache.push(calculator.currentNum);
     calculator.cache.push(target.textContent);
     calculator.currentNum = "";
+  }
+}
+
+function calculate() {
+  calculator.cache.push(calculator.currentNum);
+  calculator.currentNum = "";
+
+  let multIndex = calculator.cache.indexOf("*");
+  let divIndex = calculator.cache.indexOf("/");
+
+  while (multIndex >= 0 || divIndex >= 0) {
+    reduceCache(multIndex, divIndex, multiply, divide);
+    multIndex = calculator.cache.indexOf("*");
+    divIndex = calculator.cache.indexOf("/");
+  }
+
+  let addIndex = calculator.cache.indexOf("+");
+  let subIndex = calculator.cache.indexOf("-");
+
+  while (addIndex >= 0 || subIndex >= 0) {
+    reduceCache(addIndex, subIndex, add, substract);
+    addIndex = calculator.cache.indexOf("+");
+    subIndex = calculator.cache.indexOf("-");
+  }
+
+  calculator.currentNum = `${calculator.cache[0]}`;
+  calculator.cache.splice(0, 1);
+}
+
+function reduceCache(index1, index2, funct1, funct2) {
+  if ((index1 >= 0 && index1 < index2) || (index1 >= 0 && index2 < 0)) {
+    result = funct1(
+      +calculator.cache[index1 - 1],
+      +calculator.cache[index1 + 1],
+    );
+    calculator.cache.splice(index1 - 1, 3, result);
+  }
+  if ((index2 >= 0 && index2 < index1) || (index2 >= 0 && index1 < 0)) {
+    result = funct2(
+      +calculator.cache[index2 - 1],
+      +calculator.cache[index2 + 1],
+    );
+    calculator.cache.splice(index2 - 1, 3, result);
   }
 }
 
